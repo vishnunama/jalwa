a8aac02842b6ef05a8c207c6bd36d277eef0b202
Aman sharma <amansharma@Amans-iMac.local>
2025-04-13 23:23:10 +0530

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,8 +2,8 @@ import mysql from "mysql2/promise";
 
 const connection = mysql.createPool({
   host: "localhost",
-  user: "admin",
-  password: "password",
+  user: "rupeeclub",
+  password: "rupeeclub@123",
   database: "91club",
 });
 
